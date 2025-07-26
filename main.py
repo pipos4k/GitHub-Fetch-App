46a5651426f0f81c859d953223c0aac10ad0864f
pipos4k <kgpapadopoulos@proton.me>
2025-07-26 19:10:04 +0300

    Handle the problem for 403 error(too many requests). Fix the problem if data cant get items break and return the DataFrame.

--- main.py
+++ main.py
@@ -16,19 +16,32 @@ json_file = fetch_github_user("pipos4k")
 
 repo_name = "python"
 
-def fetch_repos(repo_name, max_repos=1000, per_page=100):
+def fetch_repos(repo_name, max_repos=2000, per_page=100):
 
     all_repos = []
     page = 1
-    search_url = f"https://api.github.com/search/repositories?q={repo_name}&per_page={per_page}&page={page}]"
 
     while len(all_repos) < max_repos:
         try:
+            search_url = f"https://api.github.com/search/repositories?q={repo_name}&per_page={per_page}&page={page}"
             response = requests.get(search_url)
-            response.raise_for_status() # Raise HTTP Error if bad response (4xx, 5xx)
+
+            print(f"Remaining: {response.headers['X-RateLimit-Remaining']}/{response.headers['X-RateLimit-Limit']}")
+            # Handle rate limits 
+            if response.status_code == 403: # 403 = too many requests
+                reset_time = int(response.headers.get("X-RateLimit-Reset", 60))
+                wait_time = max(reset_time - time.time(), 0) + 5
+                print(f"Rate limited. Waiting {wait_time:.0f} seconds...")
+                time.sleep(wait_time)
+                continue
 
             data = response.json()
             repos = data.get("items", [])
+            print(f"Page {page}: {len(data.get('items', []))} repos")  # Debug line
+    
+            if not data.get('items'):
+                print(f"Stopping: No items in response. Full response: {data}")
+                break
 
             if "items" not in data or not data["items"]:
                 print(f"No repo found for {repo_name}")
@@ -42,7 +55,6 @@ def fetch_repos(repo_name, max_repos=1000, per_page=100):
 
             # Progress update
             print(f"Fetched page {page} | Total: {len(all_repos)} repos")
-
             time.sleep(2)
 
         except RequestException as e:
@@ -57,5 +69,4 @@ def create_csv(df):
     pass
 
 data_ = fetch_repos(repo_name=repo_name)
-
 print(data_.head(20))
