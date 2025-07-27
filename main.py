28dc7788597e76a98ec734177adb10c773a5b3a3
pipos4k <kgpapadopoulos@proton.me>
2025-07-27 17:28:04 +0300

    Improve fetch_repos function, add check for valid input function and add a create_csv function to save as txt the dataframe

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from requests.exceptions import RequestException
 import time
 
 def fetch_github_user(username):
+
     url = f"https://api.github.com/users/{username}"
     response = requests.get(url)
     if response.status_code == 200:
@@ -12,21 +13,18 @@ def fetch_github_user(username):
     else:
         return None  # User not found
     
-json_file = fetch_github_user("pipos4k")
+def fetch_repos(repo_name, max_repos, per_page):
 
-repo_name = "python"
-
-def fetch_repos(repo_name, max_repos=2000, per_page=100):
-
+    print(f"Serching: {repo_name}, (Max:{max_repos}, Per page: {per_page})")
     all_repos = []
     page = 1
 
     while len(all_repos) < max_repos:
+        
         try:
             search_url = f"https://api.github.com/search/repositories?q={repo_name}&per_page={per_page}&page={page}"
             response = requests.get(search_url)
 
-            print(f"Remaining: {response.headers['X-RateLimit-Remaining']}/{response.headers['X-RateLimit-Limit']}")
             # Handle rate limits 
             if response.status_code == 403: # 403 = too many requests
                 reset_time = int(response.headers.get("X-RateLimit-Reset", 60))
@@ -60,13 +58,47 @@ def fetch_repos(repo_name, max_repos=2000, per_page=100):
         except RequestException as e:
             print(f"Network/Request error: {e}")
             return pd.DataFrame()
-        
-    return pd.DataFrame(all_repos[:max_repos])
-    # df = pd.DataFrame(data["items"]) # Find all items from repo
-    # return df.sort_values(by="forks", ascending=False)
+    
+    df = pd.DataFrame(all_repos[:max_repos])
+    
+    return df.sort_values(by="forks", ascending=False) # Return DataFame sorting by forks
 
-def create_csv(df):
-    pass
+def get_valid_input():
 
-data_ = fetch_repos(repo_name=repo_name)
-print(data_.head(20))
+    print("Enter 'Repo name' 'Max repos' 'Per page' (e.g. 'python 100 30'):")
+    while True:
+        user_input = input().strip()
+        parts = user_input.split()
+
+        # Check if input has 3 parts
+        if len(parts) != 3:
+            print("Error: Format must be 'REPO_NAME MAX_REPOS PER_PAGE'")
+            continue
+
+        repo_name, max_repos, per_page = parts
+
+        # Check for numeric 
+        if not (max_repos.isdigit() and per_page.isdigit()):
+            print("Error: MAX_REPOS and PER_PAGE must be numbers")
+            continue
+
+        return repo_name, int(max_repos), int(per_page)
+
+def create_csv(df, name_file):
+    # Save to text file with aligned columns
+    with open(f"{name_file}_github_repos.txt", "w") as f:
+        # Write header
+        f.write(f"{'Repository name':<35} {'URL' :<60} {'Forks' :10}\n")
+        f.write("-" *105 + "\n")
+
+        # Write each row
+        for _, row in df.iterrows():
+            f.write(f"{row['name']:<35} {row['html_url']:<60} {row['forks']:<10}\n")
+
+# json_file = fetch_github_user("pipos4k")
+repo_name , max_repos, per_page = get_valid_input()
+repo_data = fetch_repos(repo_name=repo_name, max_repos=int(max_repos), per_page=int(per_page))
+clear_html_repo = repo_data[["name", "html_url", "forks"]] # Create new Dataframe (more readable for urls)
+create_csv(clear_html_repo, repo_name)
+print(clear_html_repo)
+
